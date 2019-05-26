0e8d7b9a1b92e8ce92283d6eb15d45c62a1d64e9
rteja888 <ravitejatwork@gmail.com>
2019-05-26 23:06:43 +0530

    Burger with hardcoded ingredients, Stylin to be fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder';
+
+/*
+<Layout> contains other elements
+<BurgerBuilder/> is a self closing component since its a independent entity.
+*/
 
 class App extends Component {
 
   render() {
             return (
               <div >
-                <Layout></Layout>
+                <Layout>
+                  
+                  <BurgerBuilder/>
+                </Layout>
               </div>
             ); // end of return
   } // end of render
